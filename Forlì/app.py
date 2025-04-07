091e57c75090e19b3abbc10a8e5233c96c172e12
Christian-Forli <forli.christian@iisgalvanimi.edu.it>
2025-04-07 09:48:32 +0000

    esercitazione 1

--- Forlì/app.py
+++ Forlì/app.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template #importiamo la classe flask
 app = Flask(__name__) #inizializza app flask
+x = "ciao"
+lista = ["mele", "latte", "uova", "pasta", "creali"]
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    return render_template("index.html", x = x)
 
 #avvio flask
 if __name__ == '__main__':
